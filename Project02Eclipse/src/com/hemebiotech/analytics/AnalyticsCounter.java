cf285278270be5582694859814634d5d944a1481
Guillaume Cliquet <guiles62@sfr.fr>
2021-05-05 15:29:45 +0200

    add comment try

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,42 +4,23 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.*;
 
 public class AnalyticsCounter {
-	/*private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;*/
 
-	public void start() throws IOException {
-		// first get input
-		BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\guile\\Desktop\\Formation\\P2_travaux\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt"));
-		String line = reader.readLine();
+	public List<String> start() throws IOException {
 
-		int i = 0;    // set i to 0
-		/*int headCount = 0;    // counts headaches*/
-		while (line != null) {
-			i++;
-			line = reader.readLine();    // get another symptom
-			System.out.println("symptom from file: " + line);
-			/*if (line.equals("headache")) {
-				headCount++;
-
-			} else if (line.equals("rush")) {
-				rashCount++;
-			} else if (line.contains("pupils")) {
-				pupilCount++;
-			}*/
-
-
-		}
+		ReadSymptomDataFromFile symptomsData = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+		return symptomsData.GetSymptoms();
+	}
+	public List<String> sort(List<String>tri) {
+		Collections.sort(tri);
+		return tri;
+	}
 
 		// next generate output
 		FileWriter writer = new FileWriter("result.out");
 
-
-		/*writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");*/
 		writer.close();
-	}
+
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/Launcher.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Launcher.java
@@ -1,9 +1,13 @@
 package com.hemebiotech.analytics;
 
+import java.util.List;
+
 public class Launcher {
     public static void main(String args[]) throws Exception {
         AnalyticsCounter analyticsCounter = new AnalyticsCounter();
-        analyticsCounter.start();
+        List<String>arrayRecup = analyticsCounter.start();
+        System.out.println(arrayRecup);
+
 
     }
 }
