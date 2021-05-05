5a9e4a93d216d5daaeea7bd01b785cc3e405dfe6
Guillaume Cliquet <guiles62@sfr.fr>
2021-05-05 09:24:42 +0200

    add comment

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,12 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Collections;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
+	/*private static int headacheCount = 0;
 	private static int rashCount = 0;
-	private static int pupilCount = 0;
+	private static int pupilCount = 0;*/
 
 	public void start() throws IOException {
 		// first get input
@@ -17,29 +16,30 @@ public class AnalyticsCounter {
 		String line = reader.readLine();
 
 		int i = 0;    // set i to 0
-		int headCount = 0;    // counts headaches
+		/*int headCount = 0;    // counts headaches*/
 		while (line != null) {
 			i++;
+			line = reader.readLine();    // get another symptom
 			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
+			/*if (line.equals("headache")) {
 				headCount++;
-				System.out.println("number of headaches: " + headCount);
+
 			} else if (line.equals("rush")) {
 				rashCount++;
 			} else if (line.contains("pupils")) {
 				pupilCount++;
-			}
+			}*/
 
-			line = reader.readLine();    // get another symptom
+
 		}
 
 		// next generate output
 		FileWriter writer = new FileWriter("result.out");
 
 
-		writer.write("headache: " + headacheCount + "\n");
+		/*writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("dialated pupils: " + pupilCount + "\n");*/
 		writer.close();
 	}
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Launcher.java
@@ -0,0 +1,9 @@
+package com.hemebiotech.analytics;
+
+public class Launcher {
+    public static void main(String args[]) throws Exception {
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+        analyticsCounter.start();
+
+    }
+}
