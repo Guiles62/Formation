edd1ecea2701effca3cb3545a73dd7a5c79a5e27
Guillaume Cliquet <guiles62@sfr.fr>
2021-05-05 17:35:46 +0200

    add method

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,26 +1,40 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
+
 public class AnalyticsCounter {
 
-	public List<String> start() throws IOException {
+	public AnalyticsCounter() throws IOException {
+	}
+
+	public List<String> start()  {
 
 		ReadSymptomDataFromFile symptomsData = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
 		return symptomsData.GetSymptoms();
 	}
-	public List<String> sort(List<String>tri) {
+	public List<String> sort(List<String> tri) {
 		Collections.sort(tri);
 		return tri;
 	}
+	public Map<String, Integer> count (List<String> alpha){
+		Map<String, Integer> listAlpha = new TreeMap<>();
+		for( int i = 0; i<alpha.size(); i++){
+		if(listAlpha.containsKey(alpha.get(i))){
+		listAlpha.put(alpha.get(i), listAlpha.get(alpha.get(i)) + 1);
+		} else {
+			listAlpha.put(alpha.get(i), 1);
+		}
+		}
+		return listAlpha;
+	}
 
-		// next generate output
+	public void write (Map<String, Integer> writen) throws IOException {
 		FileWriter writer = new FileWriter("result.out");
-
+		writer.write(String.valueOf(writen));
 		writer.close();
-
+	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/Launcher.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Launcher.java
@@ -1,13 +1,18 @@
 package com.hemebiotech.analytics;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class Launcher {
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws IOException {
         AnalyticsCounter analyticsCounter = new AnalyticsCounter();
         List<String>arrayRecup = analyticsCounter.start();
         System.out.println(arrayRecup);
-
-
+        arrayRecup = analyticsCounter.sort(arrayRecup);
+        System.out.println(arrayRecup);
+        Map<String, Integer> mapRecup = analyticsCounter.count(arrayRecup);
+        System.out.println(mapRecup);
+        analyticsCounter.write(mapRecup);
     }
 }
