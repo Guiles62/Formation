b025abddd15d782e3689f8eb7850fc65f150227b
Guillaume Cliquet <guiles62@sfr.fr>
2021-05-07 11:51:56 +0200

    add exception + javadoc

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,24 +5,52 @@ import javax.swing.text.html.HTMLDocument;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
-
+/**
+ * <b>la classe AnalyticsCounter est celle qui contient la corp du code</b>
+ * <p>
+ *     on y retrouve :
+ *     <ul>
+ *         <li>la fonction start</li>
+ *         <li>la fonction sort</li>
+ *         <li>la fonction count</li>
+ *         <li>la fonction write</li>
+ *     </ul>
+ * </p>
+ * @see ReadSymptomDataFromFile
+ * @author Guiles
+ */
 
 public class AnalyticsCounter {
 
-	public AnalyticsCounter() throws IOException {
-	}
-
+	/**
+	* c'est le point de départ du programme
+	* on va chercher la class ReadSympomDataFromFile et on l'instancie ici en lui indiquant le chemin du .txt
+	* @return le ArrayList créé a partir du fichier.txt
+	*/
 	public List<String> start()  {
-
 		ReadSymptomDataFromFile symptomsData = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
 		return symptomsData.GetSymptoms();
 	}
+
+	/**
+	 * @param tri
+	 * 			Le nom de la liste triée
+	 * on tri le ArrayList par ordre alphabetique
+	 * @return le Arraylist trié alphabétiquement
+	 */
 	public List<String> sort(List<String> tri) {
 		Collections.sort(tri);
 		return tri;
 	}
+	/**
+	 * @param alpha
+	 * on copie le array sur un treemap
+	 * @return le Map trié par ordre alphabetique et comptabilisé
+	 */
 	public Map<String, Integer> count (List<String> alpha){
+		//on crée le TreeMap
 		Map<String, Integer> listAlpha = new TreeMap<>();
+		// on comptabilise les symtomes en les ajoutant au TreeMap
 		for( int i = 0; i<alpha.size(); i++){
 		if(listAlpha.containsKey(alpha.get(i))){
 		listAlpha.put(alpha.get(i), listAlpha.get(alpha.get(i)) + 1);
@@ -32,11 +60,20 @@ public class AnalyticsCounter {
 		}
 		return listAlpha;
 	}
-	public void write (Map<String, Integer> writen) throws IOException {
-		FileWriter writer = new FileWriter("result.out");
-		for (Map.Entry<String, Integer> entry : writen.entrySet()) {
-		writer.write((entry.getKey() + " : " + entry.getValue()) +"\n");
+	/** @param writen
+	 * on écrit le fichier de sortie en y incluant les exceptions
+	 */
+	public void write (Map<String, Integer> writen)  {
+		try {
+			FileWriter writer = new FileWriter("result.out");
+			// on prend les index les uns après les autres
+			for (Map.Entry<String, Integer> entry : writen.entrySet()) {
+			// on écrit chaque clé et valeur avec un /n
+				writer.write((entry.getKey() + " : " + entry.getValue()) + "\n");
+			}
+			writer.close();
+		} catch ( IOException exception) {
+			System.out.println("Le fichier n'a pas pu être écrit ");
 		}
-		writer.close();
 	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/Launcher.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Launcher.java
@@ -3,16 +3,43 @@ package com.hemebiotech.analytics;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-
+/**
+ * <b>la classe launcher est celle qui contient la fonction main</b>
+ * <p>
+ *     on y retrouve :
+ *     <ul>
+ *         <li>la fonction start</li>
+ *         <li>la fonction sort</li>
+ *         <li>la fonction count</li>
+ *         <li>la fonction write</li>
+ *     </ul>
+ * </p>
+ * @see AnalyticsCounter
+ * @author Guiles
+*/
 public class Launcher {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
+
+        // @param instantiation de la class AnalyticsCounter
         AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+
+        /** @param instanciation de la methode start
+        * on va chercher les informations du ArrayList*/
         List<String>arrayRecup = analyticsCounter.start();
         System.out.println(arrayRecup);
+
+        /** @param instanciation de la methode sort
+        * on va chercher la list triée par ordre alpha */
         arrayRecup = analyticsCounter.sort(arrayRecup);
         System.out.println(arrayRecup);
+
+        /** @param instanciation de la methode count
+        * on va chercher le treemap compté */
         Map<String, Integer> mapRecup = analyticsCounter.count(arrayRecup);
         System.out.println(mapRecup);
+
+        /** @param instanciation de la methode write
+        * on écrit la fichier de sortie */
         analyticsCounter.write(mapRecup);
     }
 }
