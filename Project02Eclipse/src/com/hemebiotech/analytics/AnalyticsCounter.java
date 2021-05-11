6d669636a22fb45c862649015e17936cb0e6cd23
Guillaume Cliquet <guiles62@sfr.fr>
2021-05-11 08:46:59 +0200

    add javadoc En

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -70,7 +70,7 @@ public class AnalyticsCounter {
 			FileWriter writer = new FileWriter("result.out");
 			// on prend les index les uns après les autres
 			for (Map.Entry<String, Integer> entry : writen.entrySet()) {
-			// on écrit chaque clé et valeur avec un /n
+			// on écrit chaque clé et valeur avec un \n
 				writer.write((entry.getKey() + " : " + entry.getValue()) + "\n");
 			}
 			writer.close();
